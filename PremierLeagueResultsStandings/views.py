c3ff4b6d332becbe2300f28e3f09e1f5841946fb
kolod <Pikoloda@student.wzsib.edu.pl>
2024-02-04 16:22:56 +0100

    Dodano requirement.txt oraz paginacje

--- PremierLeagueResultsStandings/views.py
+++ PremierLeagueResultsStandings/views.py
@@ -5,14 +5,28 @@ from django.contrib.auth import login, logout, authenticate
 from django.urls import reverse
 from django.contrib.auth.views import LoginView
 from .forms import AddTeamForm, StatsForm, NotesForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
+# def seasons_teams(request):
+#     teams = Team.objects.all()
+#     return render(request, 'seasons_teams.html', {
+#         'teams': teams,
+#         'user': request.user
+#     })
 
 def seasons_teams(request):
-    teams = Team.objects.all()
-    return render(request, 'seasons_teams.html', {
-        'teams': teams,
-        'user': request.user
-    })
+    teams_list = Team.objects.all()
+    paginator = Paginator(teams_list, 15)
+    page = request.GET.get('page', 1)
+    try:
+        teams = paginator.page(page)
+    except PageNotAnInteger:
+        teams = paginator.page(1)
+    except EmptyPage:
+        teams = paginator.page(paginator.num_pages)
+
+    return render(request, 'seasons_teams.html', {'teams': teams, 'user': request.user})
 
 
 def statistics_list(request):
@@ -24,7 +38,6 @@ def statistics_list(request):
 
 def team_details(request, team_id):
     team = get_object_or_404(Team, pk=team_id)
-
     if team.stats:
         stats = team.stats
     else:
@@ -34,7 +47,6 @@ def team_details(request, team_id):
         notes = team.notes
     else:
         notes = None
-
     return render(request, 'team_details.html', {'team': team, 'stats': stats, 'notes': notes})
 
 
@@ -47,16 +59,13 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-
             return redirect('seasons_teams')
     else:
         form = UserCreationForm()
-
     return render(request, 'register.html', {'form': form})
 
 
@@ -66,7 +75,6 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-
             return render(request, 'seasons_teams.html')
     else:
         form = AuthenticationForm()
@@ -90,7 +98,6 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse('seasons_teams')
 
-
 login_view = CustomLoginView.as_view()
 
 
@@ -100,15 +107,12 @@ def add_team_in_season(request):
         if form.is_valid():
             season = form.cleaned_data['season']
             team_name = form.cleaned_data['team_name']
-
             if Team.objects.filter(season_End_Year=season, team=team_name).exists():
                 error_message = "Taki zespół już istnieje w tym sezonie."
                 return render(request, 'add_team_in_season.html', {'form': form, 'error_message': error_message})
-
             if Team.objects.filter(season_End_Year=season).count() >= 20:
                 error_message = "Już jest komplet drużyn w tym sezonie."
                 return render(request, 'add_team_in_season.html', {'form': form, 'error_message': error_message})
-
             last_season = Team.objects.order_by('-season_End_Year').first()
             if last_season and season < last_season.season_End_Year:
                 error_message = "Wprowadź poprawny sezon (równy lub większy niż ostatni sezon)."
@@ -125,10 +129,8 @@ def add_team_in_season(request):
             new_team.save()
 
             return redirect('add_statistics', team_id=new_team.id)
-
     else:
         form = AddTeamForm()
-
     return render(request, 'add_team_in_season.html', {'form': form})
 
 
@@ -139,18 +141,13 @@ def add_statistics(request, team_id):
         form = StatsForm(request.POST)
         if form.is_valid():
             cleaned_data = {key: value if value is not None else '' for key, value in form.cleaned_data.items()}
-
             stats, created = Stats.objects.get_or_create(team=team, defaults=cleaned_data)
-
             if not created:
                 stats.__dict__.update(cleaned_data)
                 stats.save()
-
             return redirect('add_notes', team_id=team.id)
-
     else:
         form = StatsForm()
-
     return render(request, 'add_statistics.html', {
         'form': form,
         'team': team,
@@ -164,15 +161,11 @@ def add_notes(request, team_id):
         form = NotesForm(request.POST)
         if form.is_valid():
             cleaned_data = {key: value if value is not None else '' for key, value in form.cleaned_data.items()}
-
             notes, created = Notes.objects.get_or_create(team=team, defaults=cleaned_data)
-
             if not created:
                 notes.__dict__.update(cleaned_data)
                 notes.save()
-
             return redirect('team_details', team_id=team_id)
     else:
         form = NotesForm()
-
     return render(request, 'add_notes.html', {'form': form, 'team': team})
